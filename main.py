ea8e3a2e46865d91689d4a2495e1db37550aa995
Simon <simon.fleissig@stud.h-da.de>
2022-11-17 18:11:19 +0100

    clear

--- main.py
+++ main.py
@@ -15,34 +15,19 @@ import numpy as np
 import random
 import spacy
 
-vocab_size = 1000
-embedding_dim = 16
-max_length = 50
+vocab_size = 10000
+embedding_dim = 128
+max_length = 250
 trunc_type = 'post'
 padding_type = 'post'
 oov_tok = "<OOV>"
-training_size = 6000
+training_size = 16000 #5000=0.88,7500=0.92,8000=0.92
 num_epochs = 10
-classes = ["No Hatespeech", "Hatespeech"]
-class_to_index = dict((c, i) for i, c in enumerate(classes))
-index_to_Class = dict((v, k) for k, v in class_to_index.items())
 
+df = pd.read_csv("korrigierte_trainingsdaten_ohne_germeval2018.csv",encoding="utf8",sep=";")
+print(df.shape)
 
-df = pd.read_csv('trainingsdaten.csv', encoding="utf-8", sep=';')
-
-df2 = pd.read_csv("Daten.csv", encoding="utf-8", sep=";")
-
-df.drop('c_id', axis=1, inplace=True)
-df.drop('date', axis=1, inplace=True)
-df.drop('author_id', axis=1, inplace=True)
-df.drop('like_count', axis=1, inplace=True)
-df.drop('quote_count', axis=1, inplace=True)
-df.drop('retweet_count', axis=1, inplace=True)
-df.drop('reply_count', axis=1, inplace=True)
-df.drop('toxi', axis=1, inplace=True)
-df.drop('target', axis=1, inplace=True)
-
-text = df['c_text'].values.tolist()
+text = df["Tweets"].values.tolist()
 newtext = []
 
 for row in text:
@@ -58,58 +43,18 @@ for row in text:
             list.remove(word)
     newtext.append(" ".join(list))
 
-# for row in df2["text"].values:
-#     newtext.append(row)
 
-labels = df['hatespeech'].values.tolist()
-
-# for row in df2["label"]:
-#     labels.append(int(row))
-
-nlp = spacy.load("de_core_news_lg")
-
-lemmalist = []
-
-# ---------------------Lemma Liste Abspeichern
-# for row in newtext:
-#     doc = nlp(row)
-#     list = []
-#     for token in doc:
-#         list.append(token.lemma_.lower())
-#     lemmalist.append(" ".join(list))
-# lemmalistclean = []
-#
-# for row in lemmalist:
-#     text = re.sub('https?://\S+|www.\S+', '', row)
-#     text = re.sub('\n', '', text)
-#     text = re.sub('\r', '', text)
-#     lemmalistclean.append(text)
-# print(lemmalistclean)
-# dict = {"Text": lemmalistclean}
-# df = pd.DataFrame(dict)
-# df.to_csv("Lemmalist3.csv")
-# -----------------------------------
-
-
-# df = pd.read_csv('Lemmalist3.csv', encoding="utf-8", sep=',')
-# df.drop('Unnamed: 0', axis=1, inplace=True)
-#
-# text = []
-#
-# for row in df['Text'].values.tolist():
-#     text.append(str(row))
+labels = df['Hatespeech'].values.tolist()
 
 text = newtext
 
-tokenizer = Tokenizer(oov_token="<OOV>")
+tokenizer = Tokenizer(vocab_size)
 tokenizer.fit_on_texts(text)
 word_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(text)
 
-padded = pad_sequences(sequences, padding='post')
-# print(padded[0])
-# print(padded.shape)
+padded = pad_sequences(sequences, padding='post',maxlen=250)
 
 training_sentences = text[0:training_size]
 testing_sentences = text[training_size:]
@@ -132,39 +77,24 @@ training_labels = np.array(training_labels)
 testing_padded = np.array(testing_padded)
 testing_labels = np.array(testing_labels)
 
-# model = tf.keras.Sequential([
-#     tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
-#     tf.keras.layers.GlobalAveragePooling1D(),
-#     tf.keras.layers.Dense(4, activation='relu'),
-#     tf.keras.layers.Dense(1, activation='sigmoid')
-# ])
-# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Embedding(10000, 16, input_length=max_length),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20, return_sequences=True)),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20)),
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
-model.compile(
-    loss='binary_crossentropy',
-    optimizer='adam',
-    metrics=['accuracy']
-)
+model.compile(loss='binary_crossentropy', optimizer=keras.optimizers.Adam(learning_rate=0.005), metrics=['accuracy'])
 
 history = model.fit(training_padded, training_labels, epochs=num_epochs,
-                    validation_data=(testing_padded, testing_labels), verbose=2)
+                    validation_data=(testing_padded, testing_labels), verbose=2,batch_size=128)
 
-# trainingsDaten =pd.read_csv('germeval2018.training.txt', encoding='utf8', sep='\t')
 
 df2 = pd.read_csv('germeval2018.training.txt', encoding="utf-8", sep='\t', names=('TEXT', 'TRASH1', 'TRASH2'))
 labelsTRAINING = df2['TRASH1'].values.tolist()
 df2.drop('TRASH1', axis=1, inplace=True)
 df2.drop('TRASH2', axis=1, inplace=True)
 
-# df2 = pd.read_csv("GermevalLemma.csv", encoding="utf-8", sep=",")
-# df2.drop('Unnamed: 0', axis=1, inplace=True)
-
-# trainingListe = df2["Text"].values.tolist()
 trainingListe = df2["TEXT"].values.tolist()
 
 
@@ -176,25 +106,14 @@ for labels in labelsTRAINING:
     if labels != "OTHER":
         counterHATESPEECHinFILE = counterHATESPEECHinFILE + 1
 
-# results = model.predict(padded)
-# p = model.predict(np.expand_dims(padded, axis=0))
 p = model.predict(padded)
 print(p)
 
-
-
 for x in range(len(p)):
     print(str(p[x]) + "----" + str(labelsTRAINING[x]))
-
-for x in range(len(p)):
-    if p[x] > 0.5:
-        print('1' + '  <------>  ' + labelsTRAINING[x])
-
-    else:
-        print('0' + '  <------>  ' + labelsTRAINING[x])
-    if p[x] > 0.5 and labelsTRAINING[x] != 'OTHER':
+    if p[x] > 0.6 and labelsTRAINING[x] != 'OTHER':
         counterforRESULTS = counterforRESULTS + 1
-    if p[x] <= 0.5 and labelsTRAINING[x] == 'OTHER':
+    if p[x] <= 0.6 and labelsTRAINING[x] == 'OTHER':
         counterforRESULTS = counterforRESULTS + 1
 
 print(len(labelsTRAINING))
