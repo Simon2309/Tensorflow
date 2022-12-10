c8fdf81f64741979d8f37fe4c7791ceba12a5037
Simon <simon.fleissig@stud.h-da.de>
2022-12-10 13:05:45 +0100

    alles

--- main.py
+++ main.py
@@ -1,69 +1,86 @@
-import csv
-
 import pandas as pd
 import tensorflow_hub as hub
-import tensorflow_text
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from tensorflow import keras
-from tempfile import TemporaryFile
-import random
 from tqdm import tqdm
 
+# ----------------------------------------------------------------------------------------------------------------------------------
+# Der Codeabschnitt bis zu nächsten Trennlinie ist nur zum laden und vorbereiten der Daten.
+# Dieser Schritt muss nur einmal ausgeführt werden da dann die Daten in den Numpy Datein gespeichert sind
+
+# Der Universal Sentence Encoder codiert Sätze in einen embedding vektor mit 512 Elementen unabhängig von der länge des Satzes
+# Zudem wird eine Vorverabreitung der Daten von dem Model durchgeführt. Die Vorverarbeitung umfasst z.B. das entfernen von Stopwörter und Tokenisierung
 use = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
 
+# Einlesen der Testdaten in ein Panda Datenframe mit dem Encoding utf-8 und den Trennzeichen ";"
 df3 = pd.read_csv("Testdaten.csv", encoding="utf8", sep=";")
 
-# X_testdaten = []
-# for r in tqdm(df3["c_text"].values.tolist()):
-#     emb = use(r)
-#     review_emb = tf.reshape(emb, [-1]).numpy()
-#     X_testdaten.append(review_emb)
-# X_testdaten = np.array(X_testdaten)
-# np.save("X_testdaten.npy", X_testdaten)
+# Testdaten werden mithilfe von dem geladenen Model in ein Vektor umgewandelt. Das Numpyarray wird abgespeichert.
+X_testdaten = []
+for r in tqdm(df3["c_text"].values.tolist()):
+    emb = use(r)
+    review_emb = tf.reshape(emb, [-1]).numpy()
+    X_testdaten.append(review_emb)
+X_testdaten = np.array(X_testdaten)
+np.save("X_testdaten.npy", X_testdaten)
 
+# Integer um beim Splitten der Daten durchzumischen
 RANDOM_SEED = 42
 
-# df = pd.read_csv("Daten_ohne_germeval2018.csv", encoding="utf8", sep=";")
-#
-# type_one_hot = OneHotEncoder(sparse=False).fit_transform(
-#     df.Hatespeech.to_numpy().reshape(-1, 1)
-# )
-#
-# train_reviews, test_reviews, y_train, y_test = \
-#     train_test_split(
-#         df.Tweets,
-#         type_one_hot,
-#         test_size=.1,
-#         random_state=RANDOM_SEED
-#     )
-# ---------Numpy Datei speichern
-# X_train = []
-# for r in tqdm(train_reviews):
-#   emb = use(r)
-#   review_emb = tf.reshape(emb, [-1]).numpy()
-#   X_train.append(review_emb)
-# X_train = np.array(X_train)
-# np.save("X_train_Daten_ohne@_mit_stopwörter_50_50.npy",X_train)
-# X_test = []
-# for r in tqdm(test_reviews):
-#   emb = use(r)
-#   review_emb = tf.reshape(emb, [-1]).numpy()
-#   X_test.append(review_emb)
-# X_test = np.array(X_test)
-# np.save("X_test_Daten_ohne@_mit_stopwörter_50_50.npy",X_test)
-# np.save("Y_train_Daten_ohne@_mit_stopwörter_50_50.npy",y_train)
-# np.save("Y_test_Daten_ohne@_mit_stopwörter_50_50.npy",y_test)
+# Einlesen der Daten die zum trainieren des Models genutzt werden.Encoding utf-8 und den Trennzeichen ";"
+df = pd.read_csv("Daten_ohne_germeval2018.csv", encoding="utf8", sep=";")
 
-X_train = np.load("X_train_Daten_ohne@_mit_stopwörter_50_50.npy")
-X_test = np.load("X_test_Daten_ohne@_mit_stopwörter_50_50.npy")
-y_train = np.load("Y_train_Daten_ohne@_mit_stopwörter_50_50.npy")
-y_test = np.load("Y_test_Daten_ohne@_mit_stopwörter_50_50.npy")
+# Vektor der Hatespeechlabels wird umgewandelt um es mit den Tweetvektor kompatibel zu machen
+type_one_hot = OneHotEncoder(sparse=False).fit_transform(
+    df.Hatespeech.to_numpy().reshape(-1, 1)
+)
 
+# Aufteilung der eingelesen Daten in Training- und Testdaten mit dem RANOM_SEED zum Vermischen und der test_size=0.1.
+# Das heißt 10% der Daten werden zum Testen des Models verwendet
+train_reviews, test_reviews, y_train, y_test = \
+    train_test_split(
+        df.Tweets,
+        type_one_hot,
+        test_size=.1,
+        random_state=RANDOM_SEED
+    )
+# Trainingsdaten vom Model werden mithilfe von dem geladenen Model in ein Vektor umgewandelt. Das Numpyarray wird abgespeichert.
+#In den X Arrays stehen die Tweets und in dem Y Arrays stehen die Hatespeechlabels
+X_train = []
+for r in tqdm(train_reviews):
+    emb = use(r)
+    review_emb = tf.reshape(emb, [-1]).numpy()
+    X_train.append(review_emb)
+X_train = np.array(X_train)
+# Testdaten vom Model werden mithilfe von dem geladenen Model in ein Vektor umgewandelt. Das Numpyarray wird abgespeichert.
+X_test = []
+for r in tqdm(test_reviews):
+    emb = use(r)
+    review_emb = tf.reshape(emb, [-1]).numpy()
+    X_test.append(review_emb)
+X_test = np.array(X_test)
+# Abspeichern der Numpy Arrays damit dei Daten schnell neu geladen werden können.
+np.save("X_train_Daten.npy", X_train)
+np.save("X_test_Daten.npy", X_test)
+np.save("Y_train_Daten.npy", y_train)
+np.save("Y_test_Daten.npy", y_test)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------
+#Der nächste Codeabschnitt ist das Model für die Binärentscheidung ob es Hatespeech ist oder nicht
+
+# Laden der Arrays aus den Datein
+X_train = np.load("X_train_Daten.npy")
+X_test = np.load("X_test_Daten.npy")
+y_train = np.load("Y_train_Daten.npy")
+y_test = np.load("Y_test_Daten.npy")
+
+# Erstellung eines Sequential Model mit einem linearen Stack von Layern
 model = keras.Sequential()
 
+# 1. Layer:  Dense Layer mit 256 Units. In einem Dense Layer sind alle Neuronen untereinander verbunden
 model.add(
     keras.layers.Dense(
         units=256,
@@ -71,28 +88,37 @@ model.add(
         activation='relu'
     )
 )
+
+# 2. Dropoutlayer der Overfitting des Models verhindert
 model.add(
     keras.layers.Dropout(rate=0.5)
 )
 
+# 3. Neuer Dense Layer mit der Hälfte an Units
 model.add(
     keras.layers.Dense(
         units=128,
         activation='relu'
     )
 )
+# 4. Dropoutlayer der Overfitting des Models verhindert
 model.add(
     keras.layers.Dropout(rate=0.5)
 )
 
+# 5. OutputLayer mit nur 2 Units für Binärentscheidung ob es Hatespeech ist oder nicht
 model.add(keras.layers.Dense(2, activation='softmax'))
+
+# Übersetzen des Models
 model.compile(
     loss='categorical_crossentropy',
     optimizer=keras.optimizers.Adam(0.001),
     metrics=['accuracy']
 )
 
-history = model.fit(
+#Das Model wird mit den Trainings- und Testdaten trainiert. Es werden 8 Durchläufe gemacht.Aufteilung der Testdaten sind 10%.
+# Und es wird nach jedem Durchlauf neu gemischt
+model.fit(
     X_train, y_train,
     epochs=8,
     batch_size=32,
@@ -101,33 +127,30 @@ history = model.fit(
     shuffle=True
 )
 
+#Laden des Testdatenarrays
 X_testdaten = np.load("X_testdaten.npy")
-X_wordlist = np.load("X_wordlist.npy")
 
-counterHATESPEECHinModel = 0
 
+#Model predicted die Daten
 p = model.predict(X_testdaten)
 
+#Grenzwert bei dem das Model entscheidet wann es Hatespeech erkennt.
+# Wenn die Wahrscheinlichkeit 45% Hatespeech beträgt wird der Tweet als Hatespeech erkannt
 threshold = 0.45
-predictions = []
 
+#Schleife über das Ergebnisarray von dem Model. An der Stelle 1 steht die Wahrscheinlichkeit für Hatespeech
+predictions = []
 for x in range(len(p)):
     if p[x][1] > threshold:
-        counterHATESPEECHinModel = counterHATESPEECHinModel + 1
         predictions.append(1)
     if p[x][1] <= threshold:
         predictions.append(0)
-    # if threshold > p[x][1] > threshold - 0.2:
-    #     for word in X_wordlist:
-    #         if word in X_testdaten[x]:
-    #             counterHATESPEECHinModel = counterHATESPEECHinModel + 1
-    #             predictions.append(1)
-    #             break
-    #     predictions.append(0)
 
-threshold = threshold + 0.01
-
+#Speichern der Predictions in das Panda Dataframe
 df3["hatespeech"] = predictions
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Bei dem Model zur Erkennung des Toxizität Levels wird das gleiche Model benutzt.
 
 df = pd.read_csv("Entwicklungsdatenfertig.csv", encoding="utf8", sep=";")
 
@@ -143,27 +166,27 @@ train_reviews, test_reviews, y_train, y_test = \
         random_state=RANDOM_SEED
     )
 # # ---------Numpy Datei speichern
-# X_train = []
-# for r in tqdm(train_reviews):
-#   emb = use(r)
-#   review_emb = tf.reshape(emb, [-1]).numpy()
-#   X_train.append(review_emb)
-# X_train = np.array(X_train)
-# np.save("X_train_Entwicklungsdaten_ohne@.npy",X_train)
-# X_test = []
-# for r in tqdm(test_reviews):
-#   emb = use(r)
-#   review_emb = tf.reshape(emb, [-1]).numpy()
-#   X_test.append(review_emb)
-# X_test = np.array(X_test)
-# np.save("X_test_Entwicklungsdaten_ohne@.npy",X_test)
-# np.save("Y_train_Entwicklungsdaten_ohne@.npy",y_train)
-# np.save("Y_test_Entwicklungsdaten_ohne@.npy",y_test)
+X_train = []
+for r in tqdm(train_reviews):
+  emb = use(r)
+  review_emb = tf.reshape(emb, [-1]).numpy()
+  X_train.append(review_emb)
+X_train = np.array(X_train)
+np.save("X_train_Entwicklungsdaten.npy",X_train)
+X_test = []
+for r in tqdm(test_reviews):
+  emb = use(r)
+  review_emb = tf.reshape(emb, [-1]).numpy()
+  X_test.append(review_emb)
+X_test = np.array(X_test)
+np.save("X_test_Entwicklungsdaten.npy",X_test)
+np.save("Y_train_Entwicklungsdaten.npy",y_train)
+np.save("Y_test_Entwicklungsdaten.npy",y_test)
 
-X_train = np.load("X_train_Entwicklungsdaten_ohne@.npy")
-X_test = np.load("X_test_Entwicklungsdaten_ohne@.npy")
-y_train = np.load("Y_train_Entwicklungsdaten_ohne@.npy")
-y_test = np.load("Y_test_Entwicklungsdaten_ohne@.npy")
+X_train = np.load("X_train_Entwicklungsdaten.npy")
+X_test = np.load("X_test_Entwicklungsdaten.npy")
+y_train = np.load("Y_train_Entwicklungsdaten.npy")
+y_test = np.load("Y_test_Entwicklungsdaten.npy")
 
 print(X_train.shape, y_train.shape)
 
@@ -190,6 +213,7 @@ model.add(
     keras.layers.Dropout(rate=0.5)
 )
 
+#5 Outputlayer. Ein Layer für jedes Level
 model.add(keras.layers.Dense(5, activation='softmax'))
 model.compile(
     loss='categorical_crossentropy',
@@ -207,37 +231,15 @@ history = model.fit(
 )
 
 p = model.predict(X_testdaten)
+
 toxi = []
-
+#Pro Tweet stehen jetzt 5 Wahrscheinlichkeiten in dem Ergebnisarray. Hier wird geschaut welches Level am wahrscheinlichsten ist.
+# Die Stelle vom größten Wert in dem Array wird genommen
 for x in range(len(p)):
-    toxi.append(np.argmax(p[x])+1)
+    toxi.append(np.argmax(p[x]) + 1)
 
+#Abspeichern in das Panda Dataframe
 df3["toxi"] = toxi
+
+#Panda Dataframe als CSV abspeichern
 df3.to_csv("Testdaten_mit_Predictions.csv", encoding="utf8", sep=";")
-
-counter1 = 0
-counter2 = 0
-counter3 = 0
-counter4 = 0
-counter5 = 0
-
-for x in range(len(p)):
-    max = np.argmax(p[x])
-
-    if max == 0:
-        counter1 = counter1 + 1
-    if max == 1:
-        counter2 = counter2 + 1
-    if max == 2:
-        counter3 = counter3 + 1
-        print(p[x])
-    if max == 3:
-        counter4 = counter4 + 1
-    if max == 4:
-        counter5 = counter5 + 1
-
-print(str(counter1))
-print(str(counter2))
-print(str(counter3))
-print(str(counter4))
-print(str(counter5))
